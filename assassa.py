0656b81905a123b1a75fa68b89dea141e3fdcf85
Jesus Gallardo <jesus.gzambrano@alumnos.udg.mx>
2016-12-06 18:26:43 -0600

    inicio de proyecto

---
+++ assassa.py
@@ -0,0 +1,16 @@
+#sisdijidsj
+class Imagen():
+    def tamarindo(self):
+        pass
+    def escala_grises(self):
+        pass
+    def binamizacion(self):
+        pass
+
+class Sonido():
+    def reproducir(self):
+        pass
+    def volumen(self):
+        pass
+    def fourier(self):
+        pass
