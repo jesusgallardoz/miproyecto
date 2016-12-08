20812670d1b6d7593989459b29adc37ed9d74306
Jesus Gallardo <jesus.gzambrano@alumnos.udg.mx>
2016-12-08 17:53:09 -0600

    clase video

---
+++ proyecto.py
@@ -0,0 +1,22 @@
+#sisdijidsj
+class Imagen():
+    def tamarindo(self):
+        print("1,2 y 3... tamarindo")
+    def escala_grises(self):
+        pass
+    def binamizacion(self):
+        pass
+
+class Sonido():
+    def reproducir(self):
+        print("Play...")
+    def volumen(self):
+        pass
+    def fourier(self):
+        pass
+
+class Video():
+    def reproducir(self):
+        print("Play...")
+    def  pausar(slef):
+        print("pause...")
